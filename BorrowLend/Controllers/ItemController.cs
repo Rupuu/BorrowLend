ff5c0b1c83a13e7d07fed997102593d2469ca22e
pgee <pgee>
2022-10-07 09:45:33 +0300

    Message

--- BorrowLend/Controllers/ItemController.cs
+++ BorrowLend/Controllers/ItemController.cs
@@ -17,5 +17,67 @@ namespace BorrowLend.Controllers
         {
             this.db = db;
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int? id)
+        {
+            var obj = db.Items.Find(id);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Item obj)
+        {
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            db.Items.Update(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var obj = db.Items.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Item obj)
+        {
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            db.Items.Remove(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
